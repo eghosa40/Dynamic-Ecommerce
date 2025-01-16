ec7d252b15d1b1f8ada65436826b7cedd149fbcf
Eghosa Edoghogho <eghosa442@gmail.com>
2025-01-16 11:50:49 +0000

    Initial commit with new project structure

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-btn color="primary">Hello Vuetify!</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
